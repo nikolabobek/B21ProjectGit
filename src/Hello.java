b68c986a44f9422df802d4d0c5323cb29f90ba2e
nikolabobek <nikolabobek@gmail.com>
2020-11-10 12:49:45 -0500

    project is already in remote repo, commiting more changes

--- src/Hello.java
+++ src/Hello.java
@@ -2,4 +2,9 @@ public class Hello {
     //more stuff
     // Mac user and windows users are happy to see bla bla
 
+    // I have already published on GitHub.com
+    // bla bla
+    // bla bla bla
+    //changes
+
 }
